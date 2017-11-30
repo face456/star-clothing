8d34df786a3bfcb12683fc145540a244114b0384
Rollin Donkey <info@rollindonkey.com>
2017-11-30 21:59:32 +0100

    Finished first Storyboard functionality

---
+++ star-clothing/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  star-clothing
+//
+//  Created by Philipp Jahn on 30.11.17.
+//  Copyright © 2017 Rollin Donkey. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
