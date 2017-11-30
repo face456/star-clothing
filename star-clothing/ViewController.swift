b2e9ce12be2871df7764ae5ffd38bc4747bad031
Rollin Donkey <info@rollindonkey.com>
2017-11-30 19:02:40 +0100

    Initial Commit

---
+++ star-clothing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  star-clothing
+//
+//  Created by Philipp Jahn on 30.11.17.
+//  Copyright © 2017 Rollin Donkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
