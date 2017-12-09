3941a88981cd987a5c57873e8b90b114849d6d9b
Rollin Donkey <info@rollindonkey.com>
2017-12-09 11:28:23 +0100

    Completed collection views

---
+++ star-clothing/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  star-clothing
+//
+//  Created by Philipp Jahn on 09.12.17.
+//  Copyright © 2017 Rollin Donkey. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
